1430aa5687f9e1afadcd1256732f18c586d9506f
Fezz <jonnicwolf@gmail.com>
2024-08-13 12:11:30 -0400

    "Added Three.js library and replaced existing HTML content with a 3D scene, updated CSS to style the canvas element"

--- main.js
+++ main.js
@@ -1,24 +1,68 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene =  new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+})
 
-setupCounter(document.querySelector('#counter'))
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+renderer.render(scene, camera)
+
+
+const geometry = new THREE.TorusGeometry( 10,3,16,100 )
+const material = new THREE.MeshStandardMaterial( { color: 0xff69b4 } )
+const torus = new THREE.Mesh( geometry, material );
+scene.add(torus);
+
+const pointlight = new THREE.PointLight(0xffffff);
+pointlight.position.set(20,20,20);
+scene.add(pointlight);
+
+const ambientlight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientlight)
+
+const lighthelper = new THREE.PointLightHelper(pointlight)
+const gridhelper = new THREE.GridHelper(200,50)
+scene.add(lighthelper)
+scene.add(gridhelper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+
+function animate () {
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  controls.update();
+
+
+  renderer.render(scene, camera);
+};
+
+function addStar () {
+  const geometry = new THREE.SphereGeometry(0.25, 64, 64);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+  const star = new THREE.Mesh(geometry, material);
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+
+  star.position.set(x, y, z)
+  scene.add(star)
+}
+Array(200).fill().forEach(addStar)
+
+animate()
+
+
+
+
+
+
+
+
+
